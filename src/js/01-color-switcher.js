356ca968417cb0688da6c7f70a900580e7310105
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-06-02 00:00:11 +0300

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  body: document.body,
+};
+const COLOR_INTERVAL = 1000;
+let colorChangeEvent = null;
 
+refs.buttonStart.addEventListener('click', onStart);
+refs.buttonStop.addEventListener('click', onStop);
+
+function onStart() {
+  refs.buttonStart.disabled = true;
+  colorChangeEvent = setInterval(changeBackgroundCOlor, COLOR_INTERVAL);
+}
+
+function onStop() {
+  refs.buttonStart.disabled = false;
+  clearInterval(colorChangeEvent);
+}
+
+function changeBackgroundCOlor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
